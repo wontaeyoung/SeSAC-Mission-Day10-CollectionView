169773b7f3beb283cbfee91c25f9a7e0a384fbd7
Taeyoung Won <hexter1994@gmail.com>
2024-01-09 16:40:16 +0900

    Initial Commit

---
+++ SeSAC-Mission-Day10-CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC-Mission-Day10-CollectionView
+//
+//  Created by 원태영 on 1/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
