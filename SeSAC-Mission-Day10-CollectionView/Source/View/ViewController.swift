7d2e14fac794b2e65e3f0bdbf358361f4c8680c0
Taeyoung Won <hexter1994@gmail.com>
2024-01-09 17:36:30 +0900

    [Feat] 세그먼트 컨트롤 설정 로직 구현

--- SeSAC-Mission-Day10-CollectionView/Source/View/ViewController.swift
+++ SeSAC-Mission-Day10-CollectionView/Source/View/ViewController.swift
@@ -7,10 +7,14 @@
 
 import UIKit
 
-enum DomesticFilter {
-  case all
-  case domestic
-  case international
+enum DomesticFilter: String, CaseIterable {
+  case all = "모두"
+  case domestic = "국내"
+  case international = "해외"
+  
+  var name: String {
+    return self.rawValue
+  }
 }
 
 final class ViewController: UIViewController {
@@ -23,6 +27,36 @@ final class ViewController: UIViewController {
     super.viewDidLoad()
     
     headerView.configureUI()
+    configureUI()
+  }
+  
+  @objc private func segmentChanged(_ sender: UISegmentedControl) {
+    print(sender.selectedSegmentIndex)
+  }
+}
+
+// MARK: - Configure UI
+extension ViewController {
+  private func configureUI() {
+    setSegment()
+  }
+  
+  private func setSegment() {
+    let items: [DomesticFilter] = DomesticFilter.allCases
+    
+    // First, Second 삭제
+    domesticSegment.removeAllSegments()
+    
+    // 모두, 국내, 해외를 Segment에 추가하기
+    items.enumerated().forEach { (index, item) in
+      domesticSegment.insertSegment(withTitle: item.name, at: index, animated: true)
+    }
+    
+    // 시작 Segment 번호를 0으로 설정
+    domesticSegment.selectedSegmentIndex = .zero
+    
+    // segment 수정 시 segmentChanged 함수가 호출되도록 연결
+    domesticSegment.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)
   }
 }
 
