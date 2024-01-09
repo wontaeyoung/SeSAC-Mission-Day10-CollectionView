c3974054c564610b3af952156a469864b41a681c
Taeyoung Won <hexter1994@gmail.com>
2024-01-09 16:48:16 +0900

    [Style] 세그먼트, 컬렉션뷰 UI 생성, Outlet 연결

--- SeSAC-Mission-Day10-CollectionView/ViewController.swift
+++ SeSAC-Mission-Day10-CollectionView/ViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+  
+  @IBOutlet weak var domesticSegment: UISegmentedControl!
+  @IBOutlet weak var cityCollectionView: UICollectionView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
   }
-
-
 }
 
