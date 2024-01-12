ed2bcb21b76d2d8d100442faf7a4e30bb078a4b0
Taeyoung Won <hexter1994@gmail.com>
2024-01-12 18:28:53 +0900

    [Build] TravelTalk 프로젝트 Init

---
+++ TravelTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelTalk
+//
+//  Created by 원태영 on 1/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
