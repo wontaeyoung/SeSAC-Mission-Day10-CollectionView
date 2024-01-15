730873f3b239062505bee36f27d28661252fba5c
Taeyoung Won <hexter1994@gmail.com>
2024-01-15 14:28:23 +0900

    [Build] Travel Magazine 프로젝트 타겟 생성

---
+++ TravelMagazine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelMagazine
+//
+//  Created by 원태영 on 1/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
